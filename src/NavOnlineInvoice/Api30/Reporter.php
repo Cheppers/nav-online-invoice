75ef74361d953825c4f2664c9f716d90c25155fa
Bálint Dózsa <balint.dozsa@cheppers.com>
2020-10-06 16:45:51 +0200

    Add API 3.0 tokenExchange

--- src/NavOnlineInvoice/Api30/Reporter.php
+++ src/NavOnlineInvoice/Api30/Reporter.php
@@ -7,6 +7,17 @@ use NavOnlineInvoice\InvoiceOperations;
 
 class Reporter extends ReporterAbstract
 {
+    public function tokenExchange()
+    {
+        $requestXml = new TokenExchangeRequestXml($this->config);
+        $responseXml = $this->connector->post("/tokenExchange", $requestXml);
+
+        $encodedToken = (string)$responseXml->encodedExchangeToken;
+        $token = $this->decodeToken($encodedToken);
+
+        return $token;
+    }
+
     public function queryTransactionStatus($transactionId, $returnOriginalRequest = false)
     {
         $requestXml = new QueryTransactionStatusRequestXml($this->config, $transactionId, $returnOriginalRequest);
