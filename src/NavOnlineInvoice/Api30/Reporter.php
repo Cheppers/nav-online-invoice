bb7fa7e2c431be08a430627fc8b0317663e2ddbb
Bálint Dózsa <balint.dozsa@cheppers.com>
2020-10-06 16:32:49 +0200

    Add API 3.0 queryTransactionStatus

---
+++ src/NavOnlineInvoice/Api30/Reporter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NavOnlineInvoice\Api30;
+
+use NavOnlineInvoice\Abstracts\Reporter as ReporterAbstract;
+use NavOnlineInvoice\InvoiceOperations;
+
+class Reporter extends ReporterAbstract
+{
+    public function queryTransactionStatus($transactionId, $returnOriginalRequest = false)
+    {
+        $requestXml = new QueryTransactionStatusRequestXml($this->config, $transactionId, $returnOriginalRequest);
+        $responseXml = $this->connector->post("/queryTransactionStatus", $requestXml);
+
+        return $responseXml;
+    }
+
+    public function manageInvoice($invoiceOperationsOrXml, $operation = "CREATE")
+    {
+
+    }
+}
