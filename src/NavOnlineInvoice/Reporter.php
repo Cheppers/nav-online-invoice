fe0795fd7ff560f73e4d960b112ec54fcf8af282
Bálint Dózsa <balint.dozsa@cheppers.com>
2020-11-02 14:17:36 +0100

    Remove API 1.1 support

--- src/NavOnlineInvoice/Reporter.php
+++ src/NavOnlineInvoice/Reporter.php
@@ -10,10 +10,6 @@ class Reporter
             return new Api30\Reporter($config);
         }
 
-        if ($config->apiVersion === '2.0') {
-            return new Api20\Reporter($config);
-        }
-
-        return new Api11\Reporter($config);
+        return new Api20\Reporter($config);
     }
 }
