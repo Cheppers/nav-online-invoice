2f6e9cc576a2a51b90510064aaefe74d27fb8bcc
Bálint Dózsa <balint.dozsa@cheppers.com>
2021-06-22 14:09:56 +0200

    Remove API 2.0 support

--- src/NavOnlineInvoice/Config.php
+++ src/NavOnlineInvoice/Config.php
@@ -6,15 +6,6 @@ class Config
 {
     public static function factory($apiVersion, $isLive, $user, $software = null)
     {
-        if ($apiVersion === '3.0') {
-            return new Api30\Config($apiVersion, $isLive, $user, $software);
-        }
-
-        if ($apiVersion === '2.0') {
-            return new Api20\Config($apiVersion, $isLive, $user, $software);
-        }
-
-        $software = null;
-        return new Api11\Config($apiVersion, $isLive, $user, $software);
+        return new Api30\Config($apiVersion, $isLive, $user, $software);
     }
 }
--- src/NavOnlineInvoice/Reporter.php
+++ src/NavOnlineInvoice/Reporter.php
@@ -6,10 +6,6 @@ class Reporter
 {
     public static function factory(\NavOnlineInvoice\Abstracts\Config $config)
     {
-        if ($config->apiVersion === '3.0') {
-            return new Api30\Reporter($config);
-        }
-
-        return new Api20\Reporter($config);
+        return new Api30\Reporter($config);
     }
 }
